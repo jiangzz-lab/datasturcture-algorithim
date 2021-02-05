23616587e4232fb40fb0fd04c445babd58e436a7
Zhanzhi <hnzjzz@gmail.com>
2021-02-04 19:04:00 -0600

    add rainbow sort and solution to quick sort

--- src/datastructre/array/OneD/sort/QuickSort.java
+++ src/datastructre/array/OneD/sort/QuickSort.java
@@ -4,11 +4,43 @@ import java.util.Random;
 import datastructure.array.oneD.Basics;
 
 public class QuickSort {
-	public void quickSort(int[] array) {
+	/*
+	 * input: 
+	 * int[] array -- unsorted, could have duplicates
+	 * 
+	 * output: 
+	 * int[] elements in array sorted in ascending order
+	 * 
+	 * corner case: 
+	 * array is null, empty ? do nothing
+	 * 
+	 * Solution: recursive
+	 * 
+	 * quickSort(array, left, right) := quick sort array[left … right]
+	 * 
+	 * recursion rule: 
+	 * i. pick a pivot and partition array[left … right] so that 
+	 * array[left, pivotIndex - 1] are <= pivot and array[pivot + 1, right] are smaller than pivot 
+	 * ii. quickSort(array, left, pivotIndex - 1), quickSort(array, pivotIndex + 1, right)
+	 * 
+	 * TC: n = input array length
+	 * TC of every recursion call = O(n) from partition;
+	 * total recursion call# = worst case: n; average case: O(logn) 
+	 * total TC = O(n^2) worst case; O(nlogn) average case 
+	 * 
+	 * SC: SC of every recursion call = O(1) 
+	 * recursion call# along a root-to-leaf path on the recursion tree = 
+	 * worse case n; average case O(logn) 
+	 * total SC = O(n) worst case; O(logn) average case
+	 * 
+	 */
+
+	public int[] quickSort(int[] array) {
 		if (array == null || array.length <= 1) {
-			return;
+			return array;
 		}
 		quickSort(array, 0, array.length - 1);
+		return array;
 	}
 
 	void quickSort(int[] array, int left, int right) {
@@ -46,9 +78,16 @@ public class QuickSort {
 	public static void main(String[] args) {
 		QuickSort solu = new QuickSort();
 		Basics helper = new Basics();
+
+		helper.print1DArray(solu.quickSort(null)); // no elements!
+
+		helper.print1DArray(solu.quickSort(new int[0])); // no elements!
+
+		helper.print1DArray(solu.quickSort(new int[] { 3 })); // 3
+
 		int[] array = new int[] { 3, 2, 5, 8, 7, 9, 0 };
-		solu.quickSort(array);
-		helper.print1DArray(array); // 0 2 3 5 7 8 9
+		helper.print1DArray(solu.quickSort(array));
+		; // 0 2 3 5 7 8 9
 	}
 
 }
---
+++ src/datastructre/array/OneD/sort/RainbowSort.java
@@ -0,0 +1,69 @@
+package datastructre.array.OneD.sort;
+
+import datastructure.array.oneD.Basics;
+
+public class RainbowSort {
+	/*
+	 * input: int[] array
+	 * 
+	 * output: int[] array, all negative numbers to left, all 0s in the middle, all
+	 * positives to right
+	 * 
+	 * corner case: array is null, empty ? do nothing
+	 * 
+	 * Solution: linear scan and partition with 3 pointers
+	 * 
+	 * [0, zero) < 0; [zero, probe) == 0; [probe, pos] to explore; (pos, n - 1] > 0
+	 * 
+	 * in every step, case1: array[probe] < 0 ? swap(zero, probe), zero++, probe++
+	 * case2: array[probe] == 0 ? probe++ case3: array[probe] > 0 ? swap(probe,
+	 * pos), pos--
+	 * 
+	 * loop condition: probe <= pos -- still have elements to explore
+	 * 
+	 * TC : n = input array length TC of every probe = O(1), total probe n --> total
+	 * TC = O(n)
+	 * 
+	 * SC: O(1)
+	 * 
+	 */
+
+	public int[] rainbowSort(int[] array) {
+		if (array == null || array.length <= 1) {
+			return array;
+		}
+		int zero = 0;
+		int probe = 0;
+		int pos = array.length - 1;
+		while (probe <= pos) {
+			if (array[probe] < 0) {
+				swap(array, zero++, probe++);
+			} else if (array[probe] == 0) {
+				probe++;
+			} else { // array[probe] > 0
+				swap(array, probe, pos--);
+			}
+		}
+		return array;
+	}
+
+	void swap(int[] array, int left, int right) {
+		int temp = array[left];
+		array[left] = array[right];
+		array[right] = temp;
+	}
+	
+	public static void main(String[] args) {
+		RainbowSort solu = new RainbowSort();
+		Basics helper = new Basics();
+		
+		helper.print1DArray(solu.rainbowSort(null));
+		
+		helper.print1DArray(solu.rainbowSort(new int[0]));
+		
+		helper.print1DArray(solu.rainbowSort(new int[] {5})); // 5
+		
+		helper.print1DArray(solu.rainbowSort(new int[] {1, 1, 0, 0, -1, 1, 0})); 
+		// -1 0 0 0 1 1 1
+	}
+}
