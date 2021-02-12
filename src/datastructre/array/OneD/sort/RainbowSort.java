f2c1b473ab24d429b99fcb4f5af2ab6011163be9
Zhanzhi <hnzjzz@gmail.com>
2021-02-12 16:39:31 -0600

    modify array oneD rainbow sort I II III

--- src/datastructre/array/OneD/sort/RainbowSort.java
+++ src/datastructre/array/OneD/sort/RainbowSort.java
@@ -4,25 +4,31 @@ import datastructure.array.oneD.Basics;
 
 public class RainbowSort {
 	/*
-	 * input: int[] array
+	 * input: 
+	 * int[] array
 	 * 
-	 * output: int[] array, all negative numbers to left, all 0s in the middle, all
-	 * positives to right
+	 * output: 
+	 * int[] array, all negative numbers to left, all 0s in the middle, all positives to right
 	 * 
-	 * corner case: array is null, empty ? do nothing
+	 * corner case: 
+	 * array is null, empty ? do nothing
 	 * 
-	 * Solution: linear scan and partition with 3 pointers
+	 * Solution: 
+	 * linear scan and partition with 3 pointers
 	 * 
-	 * [0, zero) < 0; [zero, probe) == 0; [probe, pos] to explore; (pos, n - 1] > 0
+	 * [0, bneg) < 0; [bneg, b0) == 0; [b0, bpos] to explore; (bpos, n - 1] > 0
 	 * 
-	 * in every step, case1: array[probe] < 0 ? swap(zero, probe), zero++, probe++
-	 * case2: array[probe] == 0 ? probe++ case3: array[probe] > 0 ? swap(probe,
-	 * pos), pos--
+	 * in every step, inspect frontier = array[b0]
+	 * case1: frontier < 0 ? array[b0] = array[bneg], array[bneg] = frontier, b0++, bneg++
+	 * case2: frontier == 0 ? b0++
+	 * case3: frontier > 0 ? array[b0] = array[bpos], array[bpos] = frontier, bpos--
 	 * 
-	 * loop condition: probe <= pos -- still have elements to explore
+	 * loop condition:
+	 * probe <= pos
 	 * 
-	 * TC : n = input array length TC of every probe = O(1), total probe n --> total
-	 * TC = O(n)
+	 * TC : n = input array length 
+	 * TC of every step = O(1), total step# = total inspection# = n
+	 * total TC = O(n)
 	 * 
 	 * SC: O(1)
 	 * 
@@ -32,16 +38,19 @@ public class RainbowSort {
 		if (array == null || array.length <= 1) {
 			return array;
 		}
-		int zero = 0;
-		int probe = 0;
-		int pos = array.length - 1;
-		while (probe <= pos) {
-			if (array[probe] < 0) {
-				swap(array, zero++, probe++);
-			} else if (array[probe] == 0) {
-				probe++;
-			} else { // array[probe] > 0
-				swap(array, probe, pos--);
+		int bneg = 0;
+		int b0 = 0;
+		int bpos = array.length - 1;
+		while (b0 <= bpos) {
+			int frontier = array[b0];
+			if (frontier == 0) {
+				b0++;
+			} else if (frontier < 0) {
+				array[b0++] = array[bneg];
+				array[bneg++] = frontier;
+			} else { // frontier > 0
+				array[b0] = array[bpos];
+				array[bpos--] = frontier;
 			}
 		}
 		return array;
@@ -63,19 +72,18 @@ public class RainbowSort {
 	 * array is null, empty ? do nothing
 	 * 
 	 * Solution: 
-	 * linearly scan and distribute
+	 * linearly scan and distribute every element to the expected range
 	 * 
-	 * use k bounds to separate the input into k sections:
-	 * [0, bounds[1]] := 1s, [0, bounds[2]] := 2s, ... [0, bounds[k - 1]] := (k-1)s
-	 * [bounds[k - 1], bounds[k]] := to explore; array[bounds[k - 1]] := frontier
+	 * use k bounds to separate the input into k sections: 
+	 * [0, bounds[1]] := 1s, [0, bounds[2]] := 2s, ... [0, bounds[k - 1]] := (k-1)s 
+	 * [bounds[k - 1], bounds[k]] := to explore;
 	 * (bounds[k], input length) := ks
 	 * 
-	 * in every step,
+	 * in every step, inspect frontier = array[bounds[k - 1]]  
 	 * 
-	 * case1: frontier = k - 1 ? b[k - 1]++
-	 * case2: frontier = k ? swap(b[k - 1], b[k]), b[k]--
-	 * case3: frontier < k ? loop swap -- 
-	 * b[k - 1] -> b[k - 2], b[k - 2] -> b[k - 3] ... b[frontier] -> frontier (original b[k - 1])
+	 * case1: frontier = k - 1 ? b[k - 1]++ 
+	 * case2: frontier = k ? swap(b[k - 1], b[k]), b[k]
+	 * case3: frontier < k ? loop swap -- b[k - 1] -> b[k - 2], b[k - 2] -> b[k - 3] ... b[frontier] -> frontier (original b[k - 1]) 
 	 * and all bounds[frontier] ... bounds[k - 1]++
 	 * 
 	 * loop condition: bounds[k - 1] <= bounds[k]
@@ -134,8 +142,8 @@ public class RainbowSort {
 
 		helper.print1DArray(solu.rainbowSort(new int[] { 1, 2, 1, 3 }, 4));
 		// 1 1 2 3
-		
+
 		helper.print1DArray(solu.rainbowSort(new int[] { 5, 1, 4, 2, 1, 3, 5, 4, 4 }, 5));
-		// 1 1 2 3 4 4 4 5 5 
+		// 1 1 2 3 4 4 4 5 5
 	}
 }
--- src/datastructre/array/OneD/sort/RainbowSortII.java
+++ src/datastructre/array/OneD/sort/RainbowSortII.java
@@ -3,45 +3,68 @@ package datastructre.array.OneD.sort;
 import datastructure.array.oneD.Basics;
 
 public class RainbowSortII {
+	/*
+	 * input: 
+	 * int[] array, contains 0, 1, 2, 3; could have duplicates
+	 * 
+	 * output: 
+	 * int[] array, sorted in the order 0-1-2-3
+	 * 
+	 * corner case: 
+	 * array is null, empty ? do nothing
+	 * 
+	 * Solution: 
+	 * linearly scan the input and distribute every element to the expected range:
+	 * 
+	 * use 4 pointers to define the ranges --
+	 * [0, b0) := 0, [b0, b1) := 1, [b1, b2) := 2, [b2, b3] := to explore, (b3, n - 1] := 3
+	 * 
+	 * in every step, inspect frontier = array[b2]
+	 * 
+	 * case1: frontier == 3 ? array[b2] = array[b3], array[b3] = frontier; b3--
+	 * case2: frontier == 2 ? b2++ 
+	 * case3: frontier == 1 ? array[b2] --> array[b1], array[b1] --> frontier; b1, b2++
+	 * case4: frontier == 0 ? array[b2] --> array[b1], array[b1] --> array[b0], array[b0] --> frontier(= 0); b0, b1, b2++
+	 * 
+	 * TC : n = input array length 
+	 * TC of every step = O(1), total step# = total inspection# = n
+	 * total TC = O(n)
+	 * 
+	 * SC: O(1)
+	 * 
+	 */
 	public int[] rainbowSortII(int[] array) {
 		if (array == null || array.length <= 1) {
 			return array;
 		}
-		int one = 0;
-		int two = 0;
-		int probe = 0;
-		int unknown = array.length - 1;
-		while (probe <= unknown) {
-			if (array[probe] == 0) {
-				swap(array, one, probe);
-				if (two > one) {
-					swap(array, two, probe);
-				}
-				one++;
-				two++;
-				probe++;
-			} else if (array[probe] == 1) {
-				swap(array, two++, probe++);
-			} else if (array[probe] == 2) {
-				probe++;
+		int b0 = 0;
+		int b1 = 0;
+		int b2 = 0;
+		int b3 = array.length - 1;
+		while (b2 <= b3) {
+			int frontier = array[b2];
+			if (frontier == 0) {
+				array[b2++] = array[b1];
+				array[b1++] = array[b0];
+				array[b0++] = frontier;
+			} else if (frontier == 1) {
+				array[b2++] = array[b1];
+				array[b1++] = frontier;
+			} else if (frontier == 2) {
+				b2++;
 			} else {
-				swap(array, probe, unknown--);
+				array[b2] = array[b3];
+				array[b3--] = frontier;
 			}
 		}
 		return array;
-	}
-
-	void swap(int[] array, int left, int right) {
-		int temp = array[left];
-		array[left] = array[right];
-		array[right] = temp;
 	}
 
 	public static void main(String[] args) {
 		RainbowSortII solu = new RainbowSortII();
 		Basics helper = new Basics();
 
-		helper.print1DArray(solu.rainbowSortII(new int[] { 1, 2, 3, 0, 2, 1, 0, 3, 3 }));
-		// 0 0 1 1 2 2 3 3 3
+		helper.print1DArray(solu.rainbowSortII(new int[] { 3, 0, 1, 2, 1, 0, 2, 0, 1, 3 }));
+		// 0 0 0 1 1 1 2 2 3 3
 	}
 }
